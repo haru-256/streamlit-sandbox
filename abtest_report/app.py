d2343bb5b68fc6682737bbcf7b2b329585fc9f26
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-09-29 09:14:03 +0900

    FIX: sampler

--- abtest_report/app.py
+++ abtest_report/app.py
@@ -3,39 +3,56 @@ import plotly.express as px
 import polars as pl
 import streamlit as st
 
-from data.data import Sampler
+from data.data import BayesianSampler, BinaryBayesianSampler, HyperParams
 
 n_samples = 10000
 
 
-def sampler_factory(seed: int, mean: float, std: float) -> Sampler:
-    return Sampler(seed=seed, mean=mean, std=std)
+def sampler_factory(seed: int, dist_type: str, prior_hyper_param: HyperParams) -> BayesianSampler:
+    if dist_type == "binary":
+        sampler = BinaryBayesianSampler(seed=seed, **prior_hyper_param)
+    else:
+        raise NotImplementedError(f"Invalid dist_type: {dist_type}")
+    return sampler
 
 
 @st.cache_data
 def sampling(
     seed: int,
-    control_params: tuple[float, float],
-    treatment_params: tuple[float, float],
+    control_params: HyperParams,
+    treatment_params: HyperParams,
     n_samples: int,
+    dist_type: str = "binary",
 ) -> pl.DataFrame:
-    control = sampler_factory(seed, mean=control_params[0], std=control_params[1])
-    treatment = sampler_factory(seed, mean=treatment_params[0], std=treatment_params[1])
+    control = sampler_factory(seed, dist_type, control_params)
+    treatment = sampler_factory(seed, dist_type, treatment_params)
     return pl.DataFrame(
         {
             "group": ["A"] * n_samples + ["B"] * n_samples,
             "value": np.concatenate(
-                [control.gen_data(n_samples), treatment.gen_data(n_samples)], axis=0
+                [control.sampling_mean(n_samples), treatment.sampling_mean(n_samples)], axis=0
             ),
         }
     )
 
 
 # Generate random data
-df = sampling(1026, (0, 1), (0.3, 1), n_samples)
+df = sampling(
+    seed=1026,
+    control_params={"alpha": 100, "beta": 50},
+    treatment_params={"alpha": 120, "beta": 30},
+    n_samples=n_samples,
+    dist_type="binary",
+).to_pandas()
 # df.pivot
 # Create a histogram
 fig = px.histogram(
-    df, x="value", marginal="box", title="A/B mean distribution", color="group", nbins=100
+    df,
+    x="value",
+    marginal="box",
+    title="A/B mean distribution",
+    color="group",
+    nbins=100,
+    barmode="overlay",
 )
 st.plotly_chart(fig)
--- abtest_report/data/data.py
+++ abtest_report/data/data.py
@@ -1,12 +1,71 @@
+from typing import Protocol, TypedDict
+
 import numpy as np
 from numpy.typing import NDArray
 
 
-class Sampler:
-    def __init__(self, seed: int, mean: float, std: float):
-        self.rng = np.random.default_rng(seed)
-        self.mean = mean
-        self.std = std
+class BayesianSampler(Protocol):
+    def update(self, data: NDArray) -> None: ...
 
-    def gen_data(self, n: int) -> NDArray[np.float64]:
-        return self.rng.normal(self.mean, self.std, n)
+    def reset(self) -> None: ...
+
+    @property
+    def trial(self) -> int: ...
+
+    def sampling_mean(self, n: int) -> NDArray: ...
+
+    def sampling_pred(self, n: int) -> NDArray: ...
+
+
+class BinaryHyperParams(TypedDict):
+    alpha: float
+    beta: float
+
+
+class BinaryBayesianSampler:
+    def __init__(self, seed: int, alpha: float, beta: float):
+        """ベイズ推定による二項分布のサンプリングを行うクラス
+
+        Args:
+            seed: _description_
+            alpha: _description_
+            beta: _description_
+        """
+        self.rng = np.random.default_rng(seed)
+        self.init_alpha = alpha
+        self.alpha = self.init_alpha
+        self.init_beta = beta
+        self.beta = self.init_beta
+        self._trials = 0
+
+    def update(self, data: NDArray) -> None:
+        assert data.ndim == 1 and np.all(np.unique(data) == [0, 1])
+
+        self._trials += len(data)
+        successes = np.sum(data)
+        failures = len(data) - successes
+
+        self.alpha = self.alpha + float(successes)
+        self.beta = self.beta + float(failures)
+
+    def reset(self) -> None:
+        self.alpha = self.init_alpha
+        self.beta = self.init_beta
+        self._trials = 0
+
+    @property
+    def p(self) -> float:
+        return self.alpha / (self.alpha + self.beta)
+
+    @property
+    def trial(self) -> int:
+        return self._trials
+
+    def sampling_mean(self, n: int) -> NDArray:
+        return self.rng.beta(self.alpha, self.beta, n)
+
+    def sampling_pred(self, n: int) -> NDArray:
+        return self.rng.binomial(1, self.p, n)
+
+
+HyperParams = BinaryHyperParams
